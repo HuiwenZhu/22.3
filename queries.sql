e6f06488c7be243c0921b345f227be05e0c18d60
HuiwenZhu <43683067+HuiwenZhu@users.noreply.github.com>
2021-11-12 14:31:49 -0800

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,14 @@
+-- write your queries here
+ SELECT * FROM owners LEFT JOIN vehicles on vehicles.owner_id=owners.id;
+ SELECT first_name, last_name , COUNT(owner_id) FROM owners o JOIN vehicles v on o.id=v.owner_id GROUP BY (first_name, last_name) ORDER BY first_name;
+ SELECT 
+  first_name, last_name, 
+  ROUND(AVG(price)) as average_price, 
+  COUNT(owner_id) 
+FROM owners o 
+JOIN vehicles v on o.id=v.owner_id 
+GROUP BY 
+  (first_name, last_name) 
+HAVING 
+  COUNT(owner_id) > 1 AND ROUND(AVG(price)) > 10000 
+ORDER BY first_name DESC;
